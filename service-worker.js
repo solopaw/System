9fd2c9bb0358df8590814eab80bc0223b71453f6
solopaw <41173209+solopaw@users.noreply.github.com>
2020-02-29 13:26:23 +1000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+var dataCacheName = 'solvedDev-v1';
+var cacheName = 'solvedDev-final-v1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/main.js',
+  '/styles/main.css'
+];
+var dataCacheName = 'solvedDev-v1';
+var cacheName = 'solvedDev-final-v1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/main.js',
+  '/styles/main.css'
+];
